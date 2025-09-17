0c0425b6fb30513099cecfab9de22464aa4066ce
Namyejun <yejun1148@gmail.com>
2025-09-17 11:05:27 +0900

    직사각형 별찍기

--- Number copy.java
+++ Number copy.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[0].length; j++) {
-                arr1[i][j] += arr2[i][j];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String answer = "";
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
             }
+            System.out.print("\n");
         }
-        return arr1;
     }
 }
