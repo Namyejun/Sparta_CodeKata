ab96e5214e6d423dfa755d89f31e594c3b6a594b
Namyejun <yejun1148@gmail.com>
2025-09-17 11:05:58 +0900

    직사각형 별찍기

--- Number17.java
+++ Number17.java
@@ -5,7 +5,6 @@ class Solution {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        String answer = "";
         for (int i = 0; i < b; i++) {
             for (int j = 0; j < a; j++) {
                 System.out.print("*");
