e96f87a255302f69ee74e39f4873d5a756987bae
Namyejun <yejun1148@gmail.com>
2025-09-17 10:54:11 +0900

    문자열 다루기 기본

---
+++ Number16.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean solution(String s) {
+        if (!(s.length() == 4 || s.length() == 6)) return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (!((int) s.charAt(i) >= (int) '0' && (int) s.charAt(i) <= (int) '9')) return false;
+        }
+        return true;
+    }
+}
