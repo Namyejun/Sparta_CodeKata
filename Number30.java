79fa99242b396c6934ef66935dfaa050f924dd2e
Namyejun <yejun1148@gmail.com>
2025-09-19 10:27:08 +0900

    정수 내림차순으로 배치하기

--- Number30.java
+++ Number30.java
@@ -1,17 +1,28 @@
+// class Solution {
+//     public long solution(long n) {
+//         String tempAnswer = "";
+//         String stringN = String.valueOf(n);
+//         int arr[] = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+//         for (int i = 0; i < stringN.length(); i++) {
+//             arr[stringN.charAt(i) - '0'] += 1;
+//         }
+
+//         for (int i = arr.length - 1; i >= 0; i--) {
+//             tempAnswer += String.valueOf(i).repeat(arr[i]); 
+//         }
+
+//         return Long.parseLong(tempAnswer);
+//     }
+// }
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
     public long solution(long n) {
-        String tempAnswer = "";
-        String stringN = String.valueOf(n);
-        int arr[] = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-        for (int i = 0; i < stringN.length(); i++) {
-            arr[stringN.charAt(i) - '0'] += 1;
-        }
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            tempAnswer += String.valueOf(i).repeat(arr[i]); 
-        }
-
-        return Long.parseLong(tempAnswer);
+        String[] t = String.valueOf(n).split("");
+        Arrays.sort(t, Comparator.reverseOrder());
+        return Long.parseLong(String.join("", t));
     }
 }
