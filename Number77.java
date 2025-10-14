686859140e20562bc43c0c09a2288a390ab489f8
Namyejun <yejun1148@gmail.com>
2025-10-14 09:55:56 +0900

    이진 변환 반복하기

---
+++ Number77.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {};
+        int remove0 = 0;
+        int times = 0;
+        while (true) {
+            if (s.equals("1")) {
+                break;
+            }
+            times += 1;
+            int ogLen = s.length();
+            s = s.replace("0","");
+            int tempS = s.length();
+            remove0 += ogLen - tempS;
+            String binaryString = Integer.toBinaryString(tempS);
+            s = binaryString;
+        }
+        answer = new int[] {times, remove0};
+        return answer;
+    }
+}
