3a0e317e8c5c675e17b23d8c1b066021361e1841
Namyejun <yejun1148@gmail.com>
2025-10-17 09:50:50 +0900

    예상 대진표

---
+++ Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 1;
+        a -= 1;
+        b -= 1;
+
+        while (a != b) {
+            a = a / 2;
+            b = b / 2;
+            
+            answer += 1;
+        }
+        return answer - 1;
+    }
+}
