b7a5251d83bd6df5bc48788546e2dbd4ec8b3db4
Namyejun <yejun1148@gmail.com>
2025-10-20 17:11:01 +0900

    귤 고르기

--- Solution.java
+++ Solution.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+
 public class Solution {
-    public int solution(int[] arr) {
-        int lcm = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            int temp = lcm;
-            while(lcm % arr[i] != 0) {
-                lcm += temp;
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        int[] map = new int[10000001];
+        Arrays.setAll(map, i -> 0);
+        for (int i = 0; i < tangerine.length; i++) {
+            map[tangerine[i]] += 1;
+        }
+        Arrays.sort(map);
+        for (int i = map.length - 1; i >= 0; i--){
+            k -= map[i];
+            if (map[i] != 0) answer += 1;
+            if (k <= 0) {
+                break;
             }
         }
-        return lcm;
+        return answer;
     }
 }
