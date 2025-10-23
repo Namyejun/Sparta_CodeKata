ca7580d22fa59798bcd0b0ffdaa69032f4a22e22
Namyejun <yejun1148@gmail.com>
2025-10-23 10:37:51 +0900

    H-index

---
+++ Number86.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations); // 오름차순 정렬
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // h편 이상 인용된 논문 수
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
