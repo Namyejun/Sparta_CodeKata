5904ca8edbfd19ed22fb576c960ecc4b3de74e0b
Namyejun <yejun1148@gmail.com>
2025-11-09 04:52:21 +0900

    2개 이하로 다른 비트

---
+++ Number101.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            long n = numbers[i];
+            if ((n & 1) == 0) {
+                answer[i] = n + 1;
+            } else {
+                long bit = (~n) & (n + 1);
+                answer[i] = n + bit - (bit >> 1);
+            }
+        }
+        return answer;
+    }
+}
