cb38013633b59b993beff4efec63fcc04b2e459e
Namyejun <yejun1148@gmail.com>
2025-11-18 11:07:39 +0900

    두 큐 합 같게 만들기

--- Number110.java
+++ Number110.java
@@ -4,27 +4,32 @@ import java.util.Deque;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = -2;
-        Deque<Integer> q1 = new ArrayDeque<>(Arrays.stream(queue1).boxed().toList());
-        Deque<Integer> q2 = new ArrayDeque<>(Arrays.stream(queue2).boxed().toList());
+        int answer = 0;
+        Deque<Long> q1 = new ArrayDeque<>();
+        Deque<Long> q2 = new ArrayDeque<>();
+
+        for (int i = 0; i < queue1.length; i++) {
+            q1.addLast(Long.valueOf(queue1[i]));
+            q2.addLast(Long.valueOf(queue2[i]));
+        }
 
         int l = q1.size() * 4;
-        int sum1 = Arrays.stream(queue1).sum();
-        int sum2 = Arrays.stream(queue2).sum();
+        long sum1 = Arrays.stream(queue1).sum();
+        long sum2 = Arrays.stream(queue2).sum();
 
         if ((sum1 + sum2) % 2 != 0) {
             return -1;
         }
         while (true) {
             if (sum1 > sum2) {
-                int out = q1.removeFirst();
+                long out = q1.removeFirst();
                 q2.addLast(out);
 
                 sum1 -= out;
                 sum2 += out;
                 answer += 1;
             } else if (sum1 < sum2) {
-                int out = q2.removeFirst();
+                long out = q2.removeFirst();
                 q1.addLast(out);
 
                 sum2 -= out;
