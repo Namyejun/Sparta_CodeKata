d1df1ac8700ab2c3c706ff919912ec40c716e461
Namyejun <yejun1148@gmail.com>
2025-11-19 16:58:43 +0900

    무인도 여행

---
+++ Number111.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String[] maps) {
+        int[] answer = {};
+        int yLen = maps.length;
+        int xLen = maps[0].length();
+
+        int[][] move = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        boolean visited = new boolean[yLen][xLen];
+
+        return answer;
+    }
+}
