e7db97785ba06c09b1bd2f02ec1e1da6f83bf2b8
Namyejun <yejun1148@gmail.com>
2025-12-03 10:48:03 +0900

    시소 짝꿍

---
+++ Number121.java
@@ -0,0 +1,31 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Map<Integer, Long> map = new HashMap<>();
+        for (int w : weights) {
+            answer += map.getOrDefault(w, 0L);
+            answer += map.getOrDefault(w * 2, 0L);
+
+            if (w % 2 == 0) {
+                answer += map.getOrDefault(w * 3 / 2, 0L);
+                answer += map.getOrDefault(w / 2, 0L);
+            }
+
+            if (w % 3 == 0) {
+                answer += map.getOrDefault(w * 2 / 3, 0L);
+                answer += map.getOrDefault(w * 4 / 3, 0L);
+            }
+
+            if (w % 4 == 0) {
+                answer += map.getOrDefault(w * 3 / 4, 0L);
+            }
+
+            map.put(w, map.getOrDefault(w, 0L) + 1);
+        }
+        return answer;
+    }
+}
