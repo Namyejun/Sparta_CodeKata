0bfc2e23a0678e2921ddf0abbffc60612daee528
Namyejun <yejun1148@gmail.com>
2025-12-04 09:47:40 +0900

    테이블 해시 함수

---
+++ Number122.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        int c = col - 1;
+        int rb = row_begin - 1;
+        int re = row_end - 1;
+        Arrays.sort(data, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[c] != o2[c] ? o1[c] - o2[c] : o2[0] - o1[0];
+            }
+        });
+        int[] s = new int[re - rb + 1];
+        for (int i = rb; i <= re; i++) {
+            int temp = 0;
+            for (int t : data[i])
+                temp += t % (i + 1);
+            s[i - rb] = temp;
+        }
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+
+        return answer;
+    }
+}
