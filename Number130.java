5ff0a9ec99ada3fa56a8d88074b6da029b44f1c9
Namyejun <yejun1148@gmail.com>
2026-01-08 00:47:20 +0900

    두 원 사이의 정수 쌍

---
+++ Number130.java
@@ -0,0 +1,14 @@
+class Solution {
+  public long solution(int r1, int r2) {
+    long answer = 0;
+
+    for (int i = 1; i <= r2; i++) {
+      long minJ = (int) Math.ceil(Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2)));
+      long maxJ = (int) Math.floor(Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2)));
+
+      answer = answer + (maxJ - minJ) + 1;
+    }
+
+    return answer * 4;
+  }
+}
