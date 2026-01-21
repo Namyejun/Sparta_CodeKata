d6e16ba883297fc21dcbef9f74243fa7eba04620
Namyejun <yejun1148@gmail.com>
2026-01-21 14:06:11 +0900

    양궁 대회

--- Number137.java
+++ Number137.java
@@ -15,25 +15,25 @@ class Solution {
     }
 
     int[] lst = new int[11];
-    dfs(0, n, lst, minInv);
+    dfs(0, n, lst, minInv); // 라이언이 쏠 수 있는 경우의 수를 만들어 놓음
 
     List<int[]> results = new ArrayList<>();
 
     for (int[] r : ryonList) {
-      results.add(new int[] { calc(info, r), Arrays.hashCode(r) });
+      results.add(new int[] { calc(info, r), Arrays.hashCode(r) }); // 경우의 수 별로 점수 차 계산, 그 점수별로 해쉬코드 부여해서 나열
     }
 
     // 점수 차 기준 정렬
     results.sort((a, b) -> b[0] - a[0]);
 
-    if (results.get(0)[0] <= 0) {
+    if (results.get(0)[0] <= 0) { // 이기는 거 불가능
       return new int[] { -1 };
     }
 
-    int maxScore = results.get(0)[0];
+    int maxScore = results.get(0)[0]; // 일단 점수 차 최대인 거
     List<int[]> candidates = new ArrayList<>();
 
-    for (int i = 0; i < results.size(); i++) {
+    for (int i = 0; i < results.size(); i++) { // 동일한 애들 선별해내기
       if (results.get(i)[0] != maxScore)
         break;
 
@@ -46,7 +46,7 @@ class Solution {
           break;
         }
       }
-      candidates.add(new int[] { minPoint, Arrays.hashCode(r) });
+      candidates.add(new int[] { minPoint, Arrays.hashCode(r) }); // 점수 낮은 애들부터
     }
 
     // 낮은 점수 더 많이 맞힌 배열 선택
